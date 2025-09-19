125f22440948d961bdca612fa2b6a22d394dc27a
albert.gou <albert.gou@cmexpro.com>
2025-09-19 11:55:37 +0800

    update and fix some code in noir-examples/basic-2

--- noir-examples/basic-2/prove.sh
+++ noir-examples/basic-2/prove.sh
@@ -1,8 +1,10 @@
 nargo compile
 nargo check
 nargo execute my-witness
-bb prove -b ./target/basic.json -w ./target/my-witness.gz -o ./target/proof
+bb prove -b ./target/basic.json -w ./target/my-witness.gz -o ./target
 echo "✅ Proof generated at ./target/proof"
-bb write_vk -b ./target/basic.json -o ./target/vk
-bb verify -k ./target/vk -p ./target/proof
+bb write_vk -b ./target/basic.json -o ./target
+bb verify -k ./target/vk -p ./target/proof -i ./target/public_inputs
 echo "✅ Verified the proof at ./target/proof"
+bb gates -b ./target/basic.json
+echo "✅ Circuit has been analyzed"
